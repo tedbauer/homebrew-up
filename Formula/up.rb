dba4429542139fddedb59e120cb2f98e4cb7c1a8
Theodore Bauer <tjb272@cornell.edu>
2024-04-22 23:44:27 -0400

    Add cargo as a dependency

--- Formula/up.rb
+++ Formula/up.rb
@@ -3,10 +3,15 @@ class Up < Formula
     homepage "https://github.com/tedbauer/up" 
     url "https://github.com/tedbauer/up/"
     version "0.1.0"
-
-    depends_on "rust"
   
     def install
+      unless which("cargo") 
+        odie <<~EOS 
+          Cargo (the Rust package manager) is required to install 'up'.
+          Please install Rust from: https://www.rust-lang.org/tools/install
+        EOS
+      end
+        
       formula_path = Pathname.new(__FILE__).expand_path
       up_path_gen_dir = formula_path.dirname.join("up-path-gen")
       cd up_path_gen_dir do
