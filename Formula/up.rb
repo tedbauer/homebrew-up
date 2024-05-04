446a914e3379178557e6bbfcaedc8292bee04d47
Theodore Bauer <tjb272@cornell.edu>
2024-05-04 14:05:44 -0400

    Update up.rb

--- Formula/up.rb
+++ Formula/up.rb
@@ -51,7 +51,7 @@ class Up < Formula
 
     <<~EOS
     To use the 'up' command, source this script in your shell profile:
-      export BINARY_PATH=$(brew --prefix)/lib/up-path-gen && source #{formula_path}/up.sh
+      export BINARY_PATH=${HOMEBREW_LIBRARY_PATH}/lib/up-path-gen && source #{formula_path}/up.sh
     Note: tab autocomplete is supported for bash only.
     EOS
     end
